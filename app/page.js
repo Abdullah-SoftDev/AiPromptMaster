38f8d664e370f7a494eb47c92b3772753d43036c
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-06-12 11:30:35 +0530

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1 className="text-center text-red-500 bg-yellow-300 text-2xl font-semibold font-mono">Lets start building AI PromptMaster</h1>
+  )
+} 
