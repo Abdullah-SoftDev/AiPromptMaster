71f1af1b63d35e449be54244e4f903b138d55be1
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-06-14 05:31:48 +0530

    Beautify the timestamp using react-moment library

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -15,7 +15,7 @@ export default function PromptCard({ index, $id, uid, tag, prompt, username, tim
     <article className="flex max-w-xl flex-col items-start justify-between bg-white rounded-lg shadow-md border border-gray-200 p-4">
       <div className="flex justify-between w-full">
         <div className="flex items-center gap-x-4 text-xs">
-          <time dateTime="2020-03-16" className="text-gray-500 font-medium">{timestamp}</time>
+          <Moment fromNow className="text-gray-500 font-medium">{timestamp}</Moment>
           <p onClick={(e) => { handleTagClick(tag) }} className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 cursor-pointer">{tag}</p>
         </div>
         <div onClick={handleCopy} className="w-10 h-10 rounded-full bg-white/10 shadow-[inset_10px_-50px_94px_0_rgb(199,199,199,0.2)] backdrop-blur flex justify-center items-center cursor-pointer">
@@ -44,7 +44,7 @@ export default function PromptCard({ index, $id, uid, tag, prompt, username, tim
           >
             Edit
           </Link>
-          <button onClick={()=>{deletePrompt($id)}}
+          <button onClick={() => { deletePrompt($id) }}
             className='text-sm  bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent cursor-pointer'
           >
             Delete
